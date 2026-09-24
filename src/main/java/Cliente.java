0d5761eb63ba648f61e7e1e745928eac5bb64ef1
gabrielcorrear-hub <gabriel.correar@uqvirtual.edu.co>
2026-09-23 21:00:06 -0500

    Descripcion de los cambios realizados

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,56 @@
+public class Cliente {
+    //Atributos
+    private String nombre;
+    private String documentoIdentidad;
+    private String telefono;
+    private String correoElectronico;
+
+    public Cliente(String nombre, String documentoIdentidad, String telefono, String correoElectronico) {
+        this.nombre = nombre;
+        this.documentoIdentidad =
+        this.telefono= telefono;
+        this.correoElectronico= correoElectronico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nombre='" + nombre + '\'' +
+                ", documentoIdentidad='" + documentoIdentidad + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", correoElectronico='" + correoElectronico + '\'' +
+                '}';
+    }
+}
--- src/main/java/aplicacion.java
+++ src/main/java/aplicacion.java
@@ -1,3 +1,3 @@
 public class aplicacion {
-    System.out.print("penfp");
+
 }

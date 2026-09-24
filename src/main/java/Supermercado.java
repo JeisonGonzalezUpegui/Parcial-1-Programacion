4a5698544621e4f717291aed502d7072512c87c4
gabrielcorrear-hub <gabriel.correar@uqvirtual.edu.co>
2026-09-23 21:50:12 -0500

    Descripcion de los cambios realizados

--- src/main/java/Supermercado.java
+++ src/main/java/Supermercado.java
@@ -1,22 +1,43 @@
 public class Supermercado {
 
-}
-//Metodos
-public boolean agregarCliente(Cliente cliente){
-    boolean agregado=false;
-    boolean existe=verificarCliente(cliente.getDocumentoIdentidad());
-    if(existe==false){
-        listaClientes.add(cliente);
-        agregado= true;
+    //Metodos
+    public boolean verificarCliente(String documentoIdentidad) {
+        boolean existe= false;
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getDocumentoIdentidad().equals(documentoIdentidad)){
+                existe=true;
+                break;
+            }
+        }
+        return existe;
     }
-    return agregado;
-}
-public boolean agregarProducto(Producto producto){
-    boolean agregado=false;
-    boolean existe=verficarProducto(producto.getCodigo());
-    if(existe==false){
-        listaProductos.add(producto);
-        agregado=true;
+    public boolean agregarCliente(Cliente cliente) {
+        boolean agregado = false;
+        boolean existe = verificarCliente(cliente.getDocumentoIdentidad());
+        if (existe == false) {
+            listaClientes.add(cliente);
+            agregado = true;
+        }
+        return agregado;
     }
-    return agregado;
+    public boolean verificarProducto(String codigo){
+        boolean existe=false;
+        for(Producto producto: listaProductos){
+            if(producto.getProducto().equals(codigo)){
+                existe=true;
+                break;
+            }
+        }
+        return existe;
+    }
+
+    public boolean agregarProducto(Producto producto) {
+        boolean agregado = false;
+        boolean existe = verficarProducto(producto.getCodigo());
+        if (existe == false) {
+            listaProductos.add(producto);
+            agregado = true;
+        }
+        return agregado;
+    }
 }

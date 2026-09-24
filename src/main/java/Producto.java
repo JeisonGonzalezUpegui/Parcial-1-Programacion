7e5faad3270fde13c45b97016202f41540bdd9de
JeisonGonzalezUpegui <jeisonj.gonzalezu@uqvirtual.edu.co>
2026-09-23 21:27:57 -0500

    AGREGANDO

---
+++ src/main/java/Producto.java
@@ -0,0 +1,66 @@
+public class Producto {
+    //Atributos
+    private String codigo;
+    private String nombre;
+    private double precio;
+    private int cantidadDisponible;
+    private CategoriaProducto categoria;
+
+    public Producto(String codigo, String nombre, double precio, int cantidadDisponible) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public CategoriaProducto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProducto categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", cantidadDisponible=" + cantidadDisponible +
+                ", categoria=" + categoria +
+                '}';
+    }
+}
